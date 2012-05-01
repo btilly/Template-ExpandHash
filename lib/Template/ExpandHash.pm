0a89e92db4053d8ebaec7ca66dcf2c8eedd8424a
Ben Tilly <btilly@gmail.com>
2012-05-01 13:22:31 -0700

    A basic module template.

---
+++ lib/Template/ExpandHash.pm
@@ -0,0 +1,111 @@
+package Template::ExpandHash;
+
+use 5.006;
+use strict;
+use warnings;
+
+=head1 NAME
+
+Template::ExpandHash - The great new Template::ExpandHash!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use Template::ExpandHash;
+
+    my $foo = Template::ExpandHash->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 SUBROUTINES/METHODS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Ben Tilly, C<< <btilly at gmail.com> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-template-expandhash at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Template-ExpandHash>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Template::ExpandHash
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker (report bugs here)
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Template-ExpandHash>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Template-ExpandHash>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Template-ExpandHash>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Template-ExpandHash/>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2012 Ben Tilly.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See http://dev.perl.org/licenses/ for more information.
+
+
+=cut
+
+1; # End of Template::ExpandHash
